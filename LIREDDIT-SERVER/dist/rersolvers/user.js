dccebd73a795ac5d97a81bdf9c3da7abf9a0d06d
Kesi P <kesi.promnual@gmail.com>
2022-01-04 17:54:38 +0100

    Login Resolver

--- LIREDDIT-SERVER/dist/rersolvers/user.js
+++ LIREDDIT-SERVER/dist/rersolvers/user.js
@@ -19,12 +19,76 @@ exports.UserResolver = void 0;
 const User_1 = require("../entities/User");
 const type_graphql_1 = require("type-graphql");
 const password_hash_1 = __importDefault(require("password-hash"));
+let UsernamePasswordInput = class UsernamePasswordInput {
+};
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], UsernamePasswordInput.prototype, "username", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], UsernamePasswordInput.prototype, "password", void 0);
+UsernamePasswordInput = __decorate([
+    (0, type_graphql_1.InputType)()
+], UsernamePasswordInput);
+let FieldError = class FieldError {
+};
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "field", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "message", void 0);
+FieldError = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], FieldError);
+let UserResponse = class UserResponse {
+};
+__decorate([
+    (0, type_graphql_1.Field)(() => [FieldError], { nullable: true }),
+    __metadata("design:type", Array)
+], UserResponse.prototype, "errors", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(() => User_1.User, { nullable: true }),
+    __metadata("design:type", User_1.User)
+], UserResponse.prototype, "user", void 0);
+UserResponse = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], UserResponse);
 let UserResolver = class UserResolver {
     async register(username, password, ctx) {
         const hashPassword = await password_hash_1.default.generate(password);
         const user = ctx.em.create(User_1.User, { username, password: hashPassword });
         await ctx.em.persistAndFlush(user);
         return user;
+    }
+    async login(options, ctx) {
+        const user = await ctx.em.findOne(User_1.User, { username: options.username });
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "The user doesn't exist"
+                    }
+                ]
+            };
+        }
+        const validPass = await password_hash_1.default.verify(options.password, user.password);
+        if (!validPass) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "The password isn't correct"
+                    }
+                ]
+            };
+        }
+        return { user };
     }
 };
 __decorate([
@@ -36,6 +100,14 @@ __decorate([
     __metadata("design:paramtypes", [String, String, Object]),
     __metadata("design:returntype", Promise)
 ], UserResolver.prototype, "register", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => UserResponse),
+    __param(0, (0, type_graphql_1.Arg)('options')),
+    __param(1, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
+    __metadata("design:returntype", Promise)
+], UserResolver.prototype, "login", null);
 UserResolver = __decorate([
     (0, type_graphql_1.Resolver)()
 ], UserResolver);
--- LIREDDIT-SERVER/src/rersolvers/user.ts
+++ LIREDDIT-SERVER/src/rersolvers/user.ts
@@ -1,11 +1,33 @@
 //schema is a grapsql query functions
 import { User } from "../entities/User";
 import { Mycontext } from "../types";
-import { Resolver, Mutation, Arg, Ctx } from "type-graphql";
+import { Resolver, Mutation, Arg, Ctx, InputType, Field, ObjectType } from "type-graphql";
 import passwordHash  from 'password-hash'
 
+@InputType()
+class UsernamePasswordInput {
+    @Field()
+    username: string;
+    @Field()
+    password: string;
+}
 
+@ObjectType()
+class FieldError{
+    @Field()
+    field: string;
+    @Field()
+    message: string;
+}
 
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[]
+
+    @Field(() => User, {nullable: true})
+    user?: User;
+}
 @Resolver()
 export class UserResolver {
     @Mutation(() => User)
@@ -21,5 +43,34 @@ export class UserResolver {
         return user
     }
 
-    
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() ctx: Mycontext
+        ) : Promise<UserResponse>
+    {
+        const user = await ctx.em.findOne(User, {username: options.username})
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "The user doesn't exist"
+                    }
+                ]
+            }
+        }
+        const validPass = await passwordHash.verify(options.password, user.password);
+        if (!validPass){
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "The password isn't correct"
+                    }
+                ]
+            }
+        }
+        return { user }
+    }
 }
