38f671c8a18c1b5cead58aa62ccdf4549529fa5d
Kesi P <kesi.promnual@gmail.com>
2022-01-29 16:53:15 +0100

    Next.js Query Params

--- lireddit-web/src/utils/useIsAuth.ts
+++ lireddit-web/src/utils/useIsAuth.ts
@@ -1,13 +1,14 @@
-import router from "next/router";
+import router, { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useMeQuery } from "../generated/graphql";
 
 //if user not loggin yet
 export const useIsAuth = () => {
     const [{data ,fetching}] = useMeQuery();
+    const router = useRouter()
     useEffect(() => {
     if(!fetching && !data?.me){
-        router.replace('/login')
+        router.replace('/login?next=' + router.pathname)
     }
     }, [fetching, data])
 }
